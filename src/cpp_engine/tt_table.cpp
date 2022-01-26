6745fd895e0c4652122a457b76b74c77fa10e5c4
Daniel Piper <piperdaniel1@gmail.com>
2022-01-25 17:44:21 -0800

    Implemented zorbist hash table

--- src/cpp_engine/tt_table.cpp
+++ src/cpp_engine/tt_table.cpp
@@ -3,4 +3,18 @@
 
 TT_Table::TT_Table() {
     std::cout << "Initializing transposition table..." << std::endl;
+    srand(time(NULL));
+    std::random_device rd;
+
+    std::default_random_engine generator(rd());
+
+    std::uniform_int_distribution<long long unsigned> distribution(0,0xFFFFFFFFFFFFFFFF);
+
+    for(int i=0; i<8; i++) {
+        for(int j=0; j<8; j++) {
+            for(int k=0; k<6; k++) {
+                this->hash_table[i][j][k] = distribution(generator);
+            }
+        }
+    }
 }
--- src/cpp_engine/tt_table.h
+++ src/cpp_engine/tt_table.h
@@ -2,10 +2,23 @@
 #define TT_TABLE
 
 #include <iostream>
+#include <time.h>
+#include <random>
+
+struct Entry {
+    int depth;
+    int eval;
+    int key;
+};
 
 class TT_Table {
+    private:
+    Entry * table;
+    std::uint64_t hash_table[8][8][6];
+
     public:
     TT_Table();
+    std::uint64_t get_hash(char board[8][8]);
 };
 
 #endif
