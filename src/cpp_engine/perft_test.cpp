efedc0c996078e703619786d4bb85aa0c39c6e73
Daniel Piper <piperdaniel1@gmail.com>
2022-01-28 10:44:21 -0800

    Switched maximize part of peft test to MovC

--- src/cpp_engine/perft_test.cpp
+++ src/cpp_engine/perft_test.cpp
@@ -64,22 +64,19 @@ int Perft_Test::minimize(Board * board, int depth, int alpha, int beta, bool ver
 }
 
 int Perft_Test::maximize(Board * board, int depth, int alpha, int beta, bool verbose = false) {
-    Move * move_list = board->get_legal_moves();
-    Move * curr_move = move_list;
+    std::vector<MovC> movesC;
+    board->get_legal_movC(movesC);
 
-    int game_over = this->eval.is_game_over(*board, move_list);
+    int game_over = this->eval.is_game_overC(*board, movesC);
     if (game_over != 0) {
         this->positions_evaluated++;
         if (game_over == 1) {
-            board->free_move_list(move_list);
             delete board;
             return 100000;
         } else if (game_over == 2) {
-            board->free_move_list(move_list);
             delete board;
             return -100000;
         } else if (game_over == 3) {
-            board->free_move_list(move_list);
             delete board;
             return 0;
         }
@@ -87,18 +84,17 @@ int Perft_Test::maximize(Board * board, int depth, int alpha, int beta, bool ver
 
     if (depth == 0) {
         this->positions_evaluated++;
-        int final_eval = this->eval.evaluate(*board, move_list, false);
-        board->free_move_list(move_list);
+        int final_eval = this->eval.evaluateC(*board, movesC, false);
         delete board;
         return final_eval;
     }
 
     int best_score = -100000;
     int score = 0;
-    while (curr_move != nullptr) {
+    for(MovC mov : movesC) {
         Board * next_board = new Board(*board);
 
-        next_board->push_move(curr_move);
+        next_board->push_movC(mov);
 
         score = this->minimize(next_board, depth - 1, alpha, beta, false);
 
@@ -113,11 +109,8 @@ int Perft_Test::maximize(Board * board, int depth, int alpha, int beta, bool ver
         if (beta <= alpha) {
             //break;
         }
-
-        curr_move = curr_move->next;
     }
 
-    board->free_move_list(move_list);
     delete board;
     return best_score;
 }
