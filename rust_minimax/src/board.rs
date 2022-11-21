dfae64faf4bacb735db3637b737e0882bd26b684
Daniel Piper <piperdaniel1@gmail.com>
2022-11-21 00:45:43 -0800

    move gen working on every perft on the wiki

--- rust_minimax/src/board.rs
+++ rust_minimax/src/board.rs
@@ -1579,6 +1579,16 @@ impl Board {
                         }
                     }
                 }
+
+                // It is possible to capture the piece that is checking us by 
+                // taking it with en passant.
+                let en_passant_square = self.is_en_passant_move(m);
+                if en_passant_square.is_some() {
+                    if en_passant_square.unwrap() == check_square.unwrap() 
+                       && !self.creates_discovered_attack(m) {
+                        legal_moves.push(m);
+                    }
+                }
             // We are moving a piece other than the king
             // and we are not in check
             } else {
@@ -2749,5 +2759,54 @@ mod tests {
         let mut board = Board::new_from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
         let count = perft(&mut board, 2, 2, 2);
         assert_eq!(count, 191);
+    }
+
+    #[test]
+    fn perft_pos3_d3() {
+        let mut board = Board::new_from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
+        let count = perft(&mut board, 3, 3, 2);
+        assert_eq!(count, 2812);
+    }
+
+    #[test]
+    fn perft_pos3_d4() {
+        let mut board = Board::new_from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
+        let count = perft(&mut board, 4, 4, 2);
+        assert_eq!(count, 43238);
+    }
+
+    #[test]
+    fn perft_pos3_d5() {
+        let mut board = Board::new_from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
+        let count = perft(&mut board, 5, 5, 2);
+        assert_eq!(count, 674624);
+    }
+
+    #[test]
+    fn perft_pos4_d4() {
+        let mut board = Board::new_from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1").unwrap();
+        let count = perft(&mut board, 4, 4, 2);
+        assert_eq!(count, 422333);
+    }
+
+    #[test]
+    fn perft_pos4_mirrored_d4() {
+        let mut board = Board::new_from_fen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1").unwrap();
+        let count = perft(&mut board, 4, 4, 2);
+        assert_eq!(count, 422333);
+    }
+
+    #[test]
+    fn perft_pos5_d4() {
+        let mut board = Board::new_from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+        let count = perft(&mut board, 4, 4, 2);
+        assert_eq!(count, 2_103_487);
+    }
+
+    #[test]
+    fn perft_pos6_d4() {
+        let mut board = Board::new_from_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10").unwrap();
+        let count = perft(&mut board, 4, 4, 2);
+        assert_eq!(count, 3_894_594);
     }
 }
